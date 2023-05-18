f54507f29d98768f52d21dffb1c6de2a73bc9313
Denny franklin <dennyfranklin2004@gmail.com>
2023-05-18 09:01:32 +0530

    feat: progress

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,7 +1,7 @@
 
-let profile_img = document.getElementById("profile_img")
+let profile_img = document.getElementsByClassName("profile_img")
 console.log(profile_img);
-profile_img.addEventListener("click",()=>{
+profile_img[0].addEventListener("click",()=>{
 if (localStorage.getItem("profile_details")){
     console.log("idf");
     window.location.href="profile.html"
@@ -11,3 +11,14 @@ else {
     window.open("signup.html")
 }
 })
+
+// let exercises = document.getElementsByClassName("exercises")
+// exercises[0].addEventListener("click",e=>{
+//     if(localStorage.length == 0){
+//       window.location.href="signup.html"
+//     }
+//     else{
+//         window.location.href="exercise.html"
+//   }
+// }
+//    )
